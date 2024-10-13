42f48744086d118dc63aaa6fe8e3b1de31d99875
adityasavant45 <savantaditya176@gmail.com>
2024-10-13 09:22:21 +0530

    adding Images

--- dist/Project2/script.js
+++ dist/Project2/script.js
@@ -1,10 +1,16 @@
-let main=document.querySelector("main")
-let s=""
-for(let i=1;i<=55;i++){
-    s+=`<div id="card" class="w-[200px] h-[80px] m-2 border border-slate-900 bg-red-900 ">
-    ${i}
-   </div>`
+const main = document.querySelector("main");
+let s = "";
+let arrImages=["pokemon.jpeg","download.png","third.jpg","fourth.jpg"]
+let random=0;
+for (let i = 0; i <= 54; i++) {
+    random=Math.floor(Math.random()*4)
+    console.log(random)
+    const newDiv = `
+        <div class="w-[120px] h-[120px] border-2 border-black rounded-md shadow-lg flex items-center justify-center bg-white">
+        <img class="object-cover overflow-hidden h-full w-full rounded-md"  src="${arrImages[random]}"/>
+        </div>
+    `;
+    s += newDiv;
 }
-main.innerHTML=s;
 
-
+main.innerHTML = s;
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,11 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["./dist/Project2/*.html"],
+  safelist: [
+    'w-[120px]', 'h-[120px]', 'border-2', 'border-black', 
+    'rounded-md', 'shadow-lg', 'flex', 'items-center', 
+    'justify-center', 'bg-white'
+  ],
   theme: {
     extend: {
   },
